0467931e04ca46521fbf7e9507d16d1383bbcb3b
Pawana93 <pawana.schirkeinst@googlemail.com>
2018-02-19 21:28:06 +0100

    Minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavSidebarComponent } from './nav-sidebar/nav-sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavSidebarComponent
   ],
   imports: [
     BrowserModule
