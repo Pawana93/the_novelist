acb069116fef4e5986fbb481ae1d52c847afd4fa
Pawana93 <pawana.schirkeinst@googlemail.com>
2018-02-20 07:23:05 +0100

    Deleted old nav-sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { NavSidebarComponent } from './nav-sidebar/nav-sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavSidebarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
