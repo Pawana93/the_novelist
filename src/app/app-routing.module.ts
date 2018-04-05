a527b5364f78e64d7aa7b6cac53fa2c4a228dedc
Kirschstein, Dennis <dennis.kirschstein@cstx.de>
2018-04-05 12:12:57 +0200

    Slight changes in Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { ProjectsListComponent } from './projects-list/projects-list.component';
 import { ProjectsDetailsComponent } from './projects-details/projects-details.component';
+import { ListComponent } from './list/list.component';
 
 const routes: Routes = [
     {
         path: '',
-        redirectTo: 'projects',
-        pathMatch: 'full'
+        component: ListComponent
     },
     {
         path: 'projects',
